04208778b2b7b146471f377cbc6f5798df468bc7
Nazatric <nazakat1930@gmail.com>
2026-08-21 15:17:42 +0530

    Fix missing height import causing CI build failure

--- app/src/main/java/com/riffdeck/player/ui/screen/lyrics/LyricsScreen.kt
+++ app/src/main/java/com/riffdeck/player/ui/screen/lyrics/LyricsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularWavyProgressIndicator
